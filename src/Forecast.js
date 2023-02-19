e77b36e991de718a74a8473853b944b556976640
Kristina <107880739+NinjaGexly@users.noreply.github.com>
2023-02-19 17:27:23 +0300

    Got data from api call and displayed it
    Week Day component renders forecast for the first day
    Adde functions for formatting day and date of a week day

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,25 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import WeekDay from "./WeekDay";
 import "./Forecast.css";
 
 export default function Forecast(props) {
-  function handleResponse(response) {}
+  const [loaded, setLoaded] = useState(false);
+  const [forecastData, setForecastData] = useState(null);
+  function handleResponse(response) {
+    setLoaded(true);
+    setForecastData(response.data.daily);
+  }
 
-  const apiKey = "e036b2a294859f51590o881405683ft3";
-  const apiUrl = `https://api.shecodes.io/weather/v1/forecast?lat=${props.latitude}&lon=${props.longitude}&key=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(handleResponse);
-
-  return (
-    <div className="Forecast">
-      <div className="wrap">
-        <WeekDay />
-        <WeekDay />
-        <WeekDay />
-        <WeekDay />
-        <WeekDay />
-        <WeekDay />
+  if (loaded) {
+    console.log(forecastData);
+    return (
+      <div className="Forecast">
+        <div className="wrap">
+          <WeekDay data={forecastData[0]} />
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "e036b2a294859f51590o881405683ft3";
+    const apiUrl = `https://api.shecodes.io/weather/v1/forecast?lat=${props.latitude}&lon=${props.longitude}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return null;
+  }
 }
+
+// temperature={forecastData[0].temperature.day}
+//             date={forecastData[0].time}
+//             icon={forecastData[0].condition.icon}
--- src/WeekDay.js
+++ src/WeekDay.js
@@ -2,15 +2,55 @@ import React from "react";
 
 import "./WeekDay.css";
 
-export default function WeekDay() {
+export default function WeekDay(props) {
+  let temperature = Math.round(props.data.temperature.day);
+  let icon = `images/${props.data.condition.icon}.png`;
+
+  function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    let day = days[date.getDay()];
+    return day;
+  }
+
+  function formatDate(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let day = date.getDate();
+    let months = [
+      "01",
+      "02",
+      "03",
+      "04",
+      "05",
+      "06",
+      "07",
+      "08",
+      "09",
+      "10",
+      "11",
+      "12",
+    ];
+    let month = months[date.getMonth()];
+    return `${day}/${month}`;
+  }
+
   return (
     <div className="WeekDay">
       <div>
-        <span className="day">Monday</span> <span className="date">20/02</span>
-        <div className="temperature">6° C</div>
+        <span className="day">{formatDay(props.data.time)}</span>{" "}
+        <span className="date">{formatDate(props.data.time)}</span>
+        <div className="temperature">{temperature}° C</div>
       </div>
       <div>
-        <img src="images/f-mist-day.png" alt="weather-icon" />
+        <img src={icon} alt="weather-icon" />
       </div>
     </div>
   );
